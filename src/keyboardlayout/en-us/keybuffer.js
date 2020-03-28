c46841a7dd129ae868772cec4a75101258142bcb
Inigo Jiron <pohatu678@msn.com>
2020-03-28 05:25:21 -0400

    Added en-us layout

---
+++ src/keyboardlayout/en-us/keybuffer.js
@@ -0,0 +1,137 @@
+const keylist = require('./keys.js');
+
+module.exports.KEYREADBUFFER10 = {
+  57: keylist.KEYMAPPER['CAPSLOCK'],
+}
+
+module.exports.KEYREADBUFFER204 = {
+  1: keylist.KEYMAPPER['WHEELUP'],
+  255: keylist.KEYMAPPER['WHEELDOWN'],
+}
+
+module.exports.KEYREADBUFFER251 = {
+  17: keylist.KEYMAPPER.ESC,
+  18: keylist.KEYMAPPER['`'],
+  20: keylist.KEYMAPPER.TAB,
+  22: keylist.KEYMAPPER.LEFTSHIFT,
+  23: keylist.KEYMAPPER.LEFTCTRL,
+
+  19: keylist.KEYMAPPER['1'],
+  26: keylist.KEYMAPPER.Q,
+  28: keylist.KEYMAPPER.A,
+  38: keylist.KEYMAPPER.Z,
+  31: keylist.KEYMAPPER.LEFTMETA,
+
+  16: keylist.KEYMAPPER.F1,
+  25: keylist.KEYMAPPER['2'],
+  27: keylist.KEYMAPPER.W,
+  37: keylist.KEYMAPPER.S,
+  45: keylist.KEYMAPPER.X,
+  39: keylist.KEYMAPPER.LEFTALT,
+
+  24: keylist.KEYMAPPER.F2,
+  34: keylist.KEYMAPPER['3'],
+  36: keylist.KEYMAPPER.E,
+  44: keylist.KEYMAPPER.D,
+  46: keylist.KEYMAPPER.C,
+
+  33: keylist.KEYMAPPER.F3,
+  35: keylist.KEYMAPPER['4'],
+  43: keylist.KEYMAPPER.R,
+  53: keylist.KEYMAPPER.F,
+  54: keylist.KEYMAPPER.V,
+
+  32: keylist.KEYMAPPER.F4,
+  42: keylist.KEYMAPPER['5'],
+  51: keylist.KEYMAPPER.T,
+  52: keylist.KEYMAPPER.G,
+  62: keylist.KEYMAPPER.B,
+
+  41: keylist.KEYMAPPER['6'],
+  59: keylist.KEYMAPPER.Y,
+  61: keylist.KEYMAPPER.H,
+  71: keylist.KEYMAPPER.N,
+  63: keylist.KEYMAPPER.SPACE,
+
+  40: keylist.KEYMAPPER.F5,
+  49: keylist.KEYMAPPER['7'],
+  60: keylist.KEYMAPPER.U,
+  68: keylist.KEYMAPPER.J,
+  70: keylist.KEYMAPPER.M,
+
+  48: keylist.KEYMAPPER.F6,
+  66: keylist.KEYMAPPER['8'],
+  67: keylist.KEYMAPPER.I,
+  69: keylist.KEYMAPPER.K,
+  78: keylist.KEYMAPPER[','],
+
+  56: keylist.KEYMAPPER.F7,
+  65: keylist.KEYMAPPER['9'],
+  76: keylist.KEYMAPPER.O,
+  77: keylist.KEYMAPPER.L,
+  86: keylist.KEYMAPPER['.'],
+
+  57: keylist.KEYMAPPER.F8,
+  74: keylist.KEYMAPPER['0'],
+  84: keylist.KEYMAPPER.P,
+  85: keylist.KEYMAPPER[';'],
+  94: keylist.KEYMAPPER['/'],
+  103: keylist.KEYMAPPER.RIGHTALT,
+
+  64: keylist.KEYMAPPER.F9,
+  75: keylist.KEYMAPPER["-"],
+  91: keylist.KEYMAPPER['['],
+  93: keylist.KEYMAPPER['\''],
+  119: keylist.KEYMAPPER.FN,
+
+  72: keylist.KEYMAPPER.F10,
+  83: keylist.KEYMAPPER['='],
+  92: keylist.KEYMAPPER[']'],
+  127: keylist.KEYMAPPER.COMPOSE,
+
+  80: keylist.KEYMAPPER.F11,
+
+  81: keylist.KEYMAPPER.F12,
+  73: keylist.KEYMAPPER.BACKSPACE,
+  99: keylist.KEYMAPPER['\\'],
+  107: keylist.KEYMAPPER.ENTER,
+  110: keylist.KEYMAPPER.RIGHTSHIFT,
+  135: keylist.KEYMAPPER.RIGHTCTRL,
+
+  88: keylist.KEYMAPPER['PRTSCR'],
+  89: keylist.KEYMAPPER['INSERT'],
+  90: keylist.KEYMAPPER['DELETE'],
+  109: keylist.KEYMAPPER['LEFT'],
+
+  96: keylist.KEYMAPPER['SCROLLLOCK'],
+  97: keylist.KEYMAPPER['HOME'],
+  98: keylist.KEYMAPPER['END'],
+  108: keylist.KEYMAPPER['UP'],
+  117: keylist.KEYMAPPER['DOWN'],
+
+  104: keylist.KEYMAPPER['PAUSE'],
+  105: keylist.KEYMAPPER['PAGEUP'],
+  106: keylist.KEYMAPPER['PAGEDOWN'],
+  125: keylist.KEYMAPPER['RIGHT'],
+
+  113: keylist.KEYMAPPER['NUMLOCK'],
+  114: keylist.KEYMAPPER['KP7'],
+  115: keylist.KEYMAPPER['KP4'],
+  116: keylist.KEYMAPPER['KP1'],
+  133: keylist.KEYMAPPER['KP0'],
+
+  121: keylist.KEYMAPPER['KPSLASH'],
+  122: keylist.KEYMAPPER['KP8'],
+  123: keylist.KEYMAPPER['KP5'],
+  124: keylist.KEYMAPPER['KP2'],
+
+  129: keylist.KEYMAPPER['KPASTERISK'],
+  130: keylist.KEYMAPPER['KP9'],
+  131: keylist.KEYMAPPER['KP6'],
+  132: keylist.KEYMAPPER['KP3'],
+  141: keylist.KEYMAPPER['KPDOT'],
+
+  137: keylist.KEYMAPPER['KPMINUS'],
+  138: keylist.KEYMAPPER['KPPLUS'],
+  140: keylist.KEYMAPPER['KPENTER'],
+}
